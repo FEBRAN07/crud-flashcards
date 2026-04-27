6c4e6c05c4220183e5902e4d5d21a6f4be70cc82
felipebrandes07@gmail.com <felipebrandes07@gmail.com>
2026-04-27 14:55:01 -0300

    fix: added error handling to adicionarBaralho.js

--- adicionarBaralho.js
+++ adicionarBaralho.js
@@ -3,14 +3,20 @@ import PromptSync from 'prompt-sync';
 const prompt = PromptSync();
 
 function adicionarBaralho() {
-    let len = baralhos.length;
-    let titulo = prompt('Digite o título do baralho: ').trim();
-    let ultimoId = baralhos[len - 1].id;
-    baralhos.push({
-        id: ultimoId + 1,
-        titulo: titulo,
-    });
-    console.log('Baralho adicionado com sucesso!');
+    while (true) {
+        let len = baralhos.length;
+        let titulo = prompt('Digite o título do baralho: ').trim();
+        if (titulo === '') {
+            console.log('ERRO: Digite um título válido!');
+            continue;
+        }
+        let ultimoId = baralhos[len - 1].id;
+        baralhos.push({
+            id: ultimoId + 1,
+            titulo: titulo,
+        });
+        console.log('Baralho adicionado com sucesso!');
+    }
 }
 
 export default adicionarBaralho;
