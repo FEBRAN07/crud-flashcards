40b74cec9cb2057156611b06f62516d3c75c9787
felipebrandes07@gmail.com <felipebrandes07@gmail.com>
2026-04-29 11:21:01 -0300

    refactor: refactored adicionarBaralho and listarPorBaralho functions

--- adicionarBaralho.js
+++ adicionarBaralho.js
@@ -3,26 +3,23 @@ import PromptSync from 'prompt-sync';
 const prompt = PromptSync();
 
 function adicionarBaralho() {
-    while (true) {
-        let len = baralhos.length;
-        let titulo = prompt('Digite o título do baralho: ').trim();
-        if (titulo === '') {
-            console.log('ERRO: Digite um título válido!');
-            continue;
-        }
-        //SE O NOME DO BARALHO FOR REPETIDO
-        //função de busca recebe o nome novo. 
-        // se achar, retorno negativo. se nao, push
-
-        let ultimoId = baralhos[len - 1].id;
-        baralhos.push({
-            id: ultimoId + 1,
-            titulo: titulo,
-        });
-        console.log('Baralho adicionado com sucesso!');
-        break;
+    let len = baralhos.length;
+    let titulo = prompt('Digite o título do baralho: ').trim();
+    if (titulo === '') {
+        console.log('ERRO: Digite um título válido!');
+        return;
     }
+    //SE O NOME DO BARALHO FOR REPETIDO
+    //função de busca recebe o nome novo.
+    // se achar, retorno negativo. se nao, push
+
+    let ultimoId = baralhos[len - 1].id;
+    baralhos.push({
+        id: ultimoId + 1,
+        titulo: titulo,
+    });
+    console.log('Baralho adicionado com sucesso!');
 }
 
-adicionarBaralho();
-//export default adicionarBaralho;
+//adicionarBaralho();
+export default adicionarBaralho;
--- listarPorBaralho.js
+++ listarPorBaralho.js
@@ -4,29 +4,26 @@ import PromptSync from 'prompt-sync';
 const prompt = PromptSync();
 
 function listarPorBaralho() {
-    while (true) {
-        let idBaralho = parseInt(prompt('Digite o id do baralho: ').trim());
-        let flag = false;
-        baralhos.forEach((baralho) => {
-            if (baralho.id === idBaralho) {
-                flag = true;
-            }
-        });
-        if (!flag) {
-            console.log('ERRO: Digite um id de baralho válido!');
-            break;
+    let idBaralho = parseInt(prompt('Digite o id do baralho: ').trim());
+    let flag = false;
+    baralhos.forEach((baralho) => {
+        if (baralho.id === idBaralho) {
+            flag = true;
         }
-        console.log(`Listando flashcards com o id de baralho ${idBaralho}: `);
-        flashcards.forEach((flashcard) => {
-            if (flashcard.idBaralho === idBaralho) {
-                console.log(
-                    `\n======\nId: ${flashcard.id}\nPergunta: ${flashcard.pergunta}\tResposta: ${flashcard.resposta}`
-                );
-            }
-        });
-        console.log('======');
-        break;
+    });
+    if (!flag) {
+        console.log('ERRO: Digite um id de baralho válido!');
+        return;
     }
+    console.log(`Listando flashcards com o id de baralho ${idBaralho}: `);
+    flashcards.forEach((flashcard) => {
+        if (flashcard.idBaralho === idBaralho) {
+            console.log(
+                `\n======\nId: ${flashcard.id}\nPergunta: ${flashcard.pergunta}\tResposta: ${flashcard.resposta}`
+            );
+        }
+    });
+    console.log('======');
 }
 
 //listarPorBaralho();
